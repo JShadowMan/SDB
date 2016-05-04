9a305bd25ef21aac8bacd0bfbcc1c283ce8936b5
JShadowMan <jshadowman@163.com>
2016-05-04 20:56:46 +0800

    <feat>(Core): init ci, coveralls and test

---
+++ Db.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author   ShadowMan
+ * @package  Db
+ * @license  MIT License
+ * @link     https://github.com/JShadowMan/Db
+ */
+class Db {
+    /**
+     * 
+     */
+    function __construct() {
+        
+    }
+
+    /**
+     * 
+     */
+    public static function factory() {
+        
+    }
+}
+?>
---
+++ README.md
@@ -0,0 +1,7 @@
+Db
+---
+
+A Database helper for PHP.
+
+
+[![Build Status](https://travis-ci.org/JShadowMan/Db.svg?branch=master)](https://travis-ci.org/JShadowMan/Db)
