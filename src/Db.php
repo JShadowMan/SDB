1a88e29d436b32d62990cd3809a35107fa4d9e29
JShadowMan <jshadowman@163.com>
2016-05-08 23:21:23 +0800

    <test>(test): nre start point

--- src/Db.php
+++ src/Db.php
@@ -1,27 +1,60 @@
 <?php
+
 /**
- * @author   ShadowMan
- * @package  Db
- * @license  MIT License
- * @link     https://github.com/JShadowMan/Db
+ *
+ * @author ShadowMan
  */
 class Db {
-    /**
-     * 
-     */
-    function __construct() {
-        
+    private static $_server = array();
+
+    private $_adapter = null;
+
+    private $_prefix  = null;
+
+    public function __construct($adapter, $prefix = null) {
+        $this->_adapter = $adapter;
+        $this->_prefix  = $prefix;
     }
 
-    /**
-     * 
-     */
-    public static function factory() {
-        
+    public static function factory($adapter, $prefix = null) {
+        return new Db();
     }
 
-    public function init() {
-        return 'initialized';
+    public static function server() {
+        return 'SERVER';
     }
+
+    public function getAdapter() {
+        return $this->_adapter;
+    }
+
+    public function getPrefix() {
+        return $this->_prefix;
+    }
+
+    # Database: MySQL
+    const DB_ADAPTER_MYSQL      = 'MySQL';
+
+    # Database: SQL Server
+    const DB_ADAPTER_SQL_SERVER = 'SQL_SERVER';
+
+    # Database: Oracle
+    const DB_ADAPTER_ORACLE     = 'ORACLE';
+
+    # Database: SQLite
+    const DB_ADAPTER_SQLITE     = 'SQLITE';
+
+    # CRUD Operator: Create
+    const DB_OPERATOR_CREATE    = 'CREATE';
+
+    # CRUD Operator: Read
+    const DB_OPERATOR_READ      = 'READ';
+
+    # CRUD Operator: Update
+    const DB_OPERATOR_UPDATE    = 'UPDATE';
+
+    # CRUD Operator: Delete
+    const DB_OPERATOR_DELETE    = 'DELETE';
 }
+
 ?>
--- tests/DbTest/DbTest.php
+++ tests/DbTest/DbTest.php
@@ -3,14 +3,25 @@
  * Db test case.
  */
 class DbTest extends PHPUnit_Framework_TestCase {
-    public static function db() {
-        return new \Db();
+    public static function db($adapter = Db::DB_ADAPTER_MYSQL, $prefix = null) {
+        return \Db::factory($adapter, $prefix);
     }
 
-    public function testInit() {
+    public function testServer() {
+        $this->assertEquals('SERVER', Db::server());
+    }
+
+    public function testDefaultClass() {
         $db = self::db();
 
-        $this->assertEquals('initialized', $db->init());
+        $this->assertEquals(Db::DB_ADAPTER_MYSQL, $db->getAdapter());
+        $this->assertEquals(null, $db->getPrefix());
+    }
+    public function testCustomClass() {
+        $db = self::db(Db::DB_ADAPTER_ORACLE, 'table_');
+
+        $this->assertEquals(Db::DB_ADAPTER_ORACLE, $db->getAdapter());
+        $this->assertEquals('table_', $db->getPrefix());
     }
 }
 
