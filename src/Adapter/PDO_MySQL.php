e75a5f34e8fa76ca66b0ee3683c21db25302bdab
JShadowMan <jshadowman@163.com>
2016-09-02 14:31:50 +0800

    <feat>(Adapter): adapter PDO_MySQL done

--- src/Adapter/PDO_MySQL.php
+++ src/Adapter/PDO_MySQL.php
@@ -10,6 +10,8 @@ namespace SDB\Adapter;
 use SDB\Adapter\MySQL;
 
 class PDO_MySQL extends MySQL {
+    protected $_affectedRows = null;
+
     /**
      * PDO Version
      * 
@@ -39,6 +41,57 @@ class PDO_MySQL extends MySQL {
      * @see \SDB\Adapter\MySQL::query()
      */
     public function query($query) {
-        
+        if (is_string($query) && strlen($query)) {
+            $result = $this->_instance->query($query, \PDO::FETCH_ASSOC);
+
+            if ($this->_instance->errorCode() != '00000') {
+                throw new \Exception("SDB: MySQL: {$this->_instance->errorCode()}: {$this->_instance->errorInfo()}", 1996);
+            }
+
+            if ($result instanceof \PDOStatement) {
+                if ($result->errorCode() !== '00000') {
+                    throw new \Exception("SDB: MySQL: {$result->errorCode()}: {$result->errorInfo()}", 1996);
+                }
+
+                foreach ($result as $row) {
+                    $this->_result[] = $row;
+                }
+
+                $this->_affectedRows = $result->rowCount();
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * PDO Version
+     * 
+     * @see \SDB\Adapter\MySQL::serverInfo()
+     */
+    public function serverInfo() {
+        if ($this->_connectFlag == false && $this->_instance == null) {
+            throw new \Exception('SDB: Required connect first', 1996);
+        }
+
+        return null; # PDO does't provied
+    }
+
+    /**
+     * PDO Version
+     * 
+     * @see \SDB\Adapter\MySQL::affectedRows()
+     */
+    public function affectedRows() {
+        return $this->_affectedRows;
+    }
+
+    /**
+     * PDO Version
+     * 
+     * @see \SDB\Adapter\MySQL::lastInsertId()
+     */
+    public function lastInsertId() {
+        return $this->_instance->lastInsertId();
     }
 }
